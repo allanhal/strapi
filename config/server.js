b20c08125fcf71bcf881ab51cabf747e3c4b71cf
Allan Araújo <allan.araujo@oowlish.com>
2021-05-24 19:00:42 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '922505a8fa424ba03d346f1831e68475'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9bad5442-300b-452b-a1d8-13b301d7a8ee'
+};
